589b1b350bc9dc4a634b08389636aad42e62698f
Jason R. Coombs <jaraco@jaraco.com>
2023-10-23 10:29:34 -0400

    Remove Python 3.7 compatibility from tests

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import subprocess
 from shutil import which
 import pathlib
@@ -11,19 +10,7 @@ from jaraco import envs
 
 
 env_types = pytest.mark.parametrize("VEnvCls", [envs.VirtualEnv, envs.VEnv])
-
-win37 = 'platform.system() == "Windows" and sys.version_info < (3, 8)'
-
-path_types = pytest.mark.parametrize(
-    "PathCls",
-    [
-        pytest.param(pathlib.Path, marks=pytest.mark.xfail(win37)),
-        path.Path,
-    ],
-)
-
-
-maybe_fspath = os.fspath if sys.version_info < (3, 8) else lambda x: x
+path_types = pytest.mark.parametrize("PathCls", [pathlib.Path, path.Path])
 
 
 @env_types
@@ -37,7 +24,7 @@ def test_root_pathlib(tmp_path, VEnvCls, PathCls):
 
     possible_bin_dirs = (tmp_path / ".venv/bin", tmp_path / ".venv/Scripts")
     bin_dir = next(f for f in possible_bin_dirs if f.exists())
-    expected_python = which("python", path=maybe_fspath(bin_dir))
+    expected_python = which("python", path=bin_dir)
 
     cmd = [venv.exe(), "-c", "import sys; print(sys.executable)"]
     out = subprocess.check_output(cmd, text=True, encoding='utf-8').strip()
